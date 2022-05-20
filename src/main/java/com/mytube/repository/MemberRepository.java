a4c2cb13cf185676515418148a9163bf6a55daa4
chojs23 <chojs990222@gmail.com>
2022-05-20 19:34:12 +0900

    toOne 관계 패치조인하고 나머지 컬렉션은 지연로딩했을 때 문제점 인프런답변

--- src/main/java/com/mytube/repository/MemberRepository.java
+++ src/main/java/com/mytube/repository/MemberRepository.java
@@ -25,7 +25,7 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
      * 페치 조인은 객체 그래프를 조회하는 기능이기 때문에 연관된 부모가 반드시 있어야하나, countQuery의 경우 count(u)로 조회 결과가 변경되었기 때문에 오류가 발생한 것이다.
      *
      * 따라서 fetch join이나 복잡한 쿼리의 경우 반드시 countQuery를 분리해서 사용하자.
-     *
+     * https://www.inflearn.com/questions/62217
      */
     @Query(value = "SELECT DISTINCT m FROM Member m left join fetch m.memberImage",
             countQuery = " select count(m) from Member m left join m.memberImage")
