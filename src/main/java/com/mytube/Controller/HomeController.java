ba5030e746bd09a6e671eb249759bd2829d4bb14
chojs23 <chojs990222@gmail.com>
2022-07-04 20:24:38 +0900

    add dependencies modelmapper

--- src/main/java/com/mytube/Controller/HomeController.java
+++ src/main/java/com/mytube/Controller/HomeController.java
@@ -7,19 +7,15 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.SessionAttribute;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
 
 @Controller
 @Slf4j
-public class homeController {
+public class HomeController {
 
     @GetMapping("/")
     public String home(@SessionAttribute(name = SessionConst.LOGIN_MEMBER, required = false)
-                                   Member loginMember, Model model) {
+                            Member loginMember, Model model) {
         log.info("home controller");
 
 
